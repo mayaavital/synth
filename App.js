7eca32d4b942effaf17f2c9870759547b4744435
mayaavital <162385787+mayaavital@users.noreply.github.com>
2025-04-25 18:30:32 -0700

    random

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Image,
+  Dimensions,
+  LogBox,
+} from "react-native";
+import { colors } from "./assets/colors";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable style={styles.connectSpotify} onPress={() => getSpotifyAuth()}>
+        <Image
+          source={require("assets/spotify-logo.png")}
+          style={styles.spotifyLogo}
+        />
+        <Text style={styles.text}>Login with Spotify</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.connectApple}
+        marginTop={windowHeight * 0.01}
+        onPress={() => getSpotifyAuth()}
+      >
+        <Image
+          source={require("assets/apple-music.png")}
+          style={styles.spotifyLogo}
+        />
+        <Text style={styles.text}>Login with Apple Music</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +42,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.background,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  connectSpotify: {
+    backgroundColor: colors.spotify,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: (windowHeight * 0.1) / 2,
+    height: windowHeight * 0.05,
+    width: windowWidth * 0.6,
+  },
+  connectApple: {
+    backgroundColor: colors.apple,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: (windowHeight * 0.1) / 2,
+    height: windowHeight * 0.05,
+    width: windowWidth * 0.6,
+  },
+  spotifyLogo: {
+    height: windowWidth * 0.08,
+    width: windowWidth * 0.08,
   },
 });
---
+++ assets/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  background: "#121212",
+  spotify: "#C6E1B8",
+  apple: "#F1D5EE",
+  gray: "#B3B3B3",
+  white: "#FFFFFF",
+};
