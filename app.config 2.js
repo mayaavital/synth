244cfadd6e3a332b23df664608ea614c93c9520a
csprout3 <csprout3@stanford.edu>
2025-05-21 11:43:48 -0700

    simplify xcode project

---
+++ app.config 2.js
@@ -0,0 +1,48 @@
+import 'dotenv/config';
+
+export default {
+  name: 'Synth',
+  slug: 'synth',
+  version: '1.0.1',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  scheme: 'synth',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff'
+  },
+  assetBundlePatterns: [
+    '**/*'
+  ],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.synth.app'
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff'
+    },
+    package: 'com.synth.app'
+  },
+  web: {
+    favicon: './assets/favicon.png'
+  },
+  extra: {
+    // Firebase Configuration
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    
+    // Spotify Configuration
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID || '44bc87fe29004136b77183319f56338e',
+    SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI || 'exp://10.32.120.199:8083/--/callback'
+  }
+}; 
