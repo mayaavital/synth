97f9c71763da65f8fd826504daec846b3722130b
csprout3 <csprout3@stanford.edu>
2025-05-21 12:12:12 -0700

    removed google maps api

--- app.config.js
+++ app.config.js
@@ -24,12 +24,7 @@ export default {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
       },
-      package: "com.csprout3.synth",
-      config: {
-        googleMaps: {
-          apiKey: process.env.GOOGLE_MAPS_API_KEY
-        }
-      }
+      package: "com.csprout3.synth"
     },
     web: {
       favicon: "./assets/favicon.png"
