19730ca31cf295bd6422606e30f3cb787cff0283
mayaavital <162385787+mayaavital@users.noreply.github.com>
2025-05-24 13:14:44 -0700

    transpile packages

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
     "react-native-screens",
     "react-native-reanimated",
     "expo-linear-gradient",
+    "react-native",
   ],
   webpack: (config, { isServer }) => {
     // Handle the fs module issue
@@ -44,14 +45,25 @@ const nextConfig = {
       use: ["@svgr/webpack"],
     });
 
-    // Add TypeScript support
+    // Add TypeScript support for all .ts and .tsx files
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
+      exclude:
+        /node_modules\/(?!(react-native|@react-native|react-native-.*)\/).*/,
       use: [
         {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-typescript"],
+            plugins: [
+              ["@babel/plugin-transform-flow-strip-types"],
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+              ["@babel/plugin-proposal-private-methods", { loose: true }],
+              [
+                "@babel/plugin-proposal-private-property-in-object",
+                { loose: true },
+              ],
+            ],
           },
         },
       ],
@@ -68,9 +80,9 @@ const nextConfig = {
 
     return config;
   },
-  // Add experimental features
+  // Remove experimental features that are causing warnings
   experimental: {
-    esmExternals: "loose",
+    esmExternals: false,
   },
 };
 
